d76df84c3a66e2c994d4fc54fe6e7fdfb5b068ba
David <daalbert@gmail.com>
2021-07-14 18:45:00 -0700

    phonebook database step2

--- index.js
+++ index.js
@@ -14,30 +14,6 @@ app.use(
 	morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-
-let persons = [
-    { 
-      "id": "ee920d74-d358-4057-a904-0c3f6488ac34",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "68a45f20-a896-4782-b301-2941f8130a7d",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3614a856-0e2e-4079-afaa-82f97fdb3110",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "628d3dac-1a97-454b-95eb-e7948fbfe9b1",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
 	Person.find({}).then(persons => {
 		response.json(persons)
@@ -64,8 +40,10 @@ app.post('/api/persons', (request, response) => {
 		badRequest = badRequest.concat({error: 'name missing'})
 	if(!body.number)
 		badRequest = badRequest.concat({error: 'number missing'})
+	/*
 	if(persons.some(p => p.name===body.name))
 		badRequest = badRequest.concat({error: 'name must be unique'})
+	*/
 
 	if (badRequest.length)
 		return response.status(400).json(badRequest)
